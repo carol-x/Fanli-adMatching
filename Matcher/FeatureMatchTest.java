aab6b8675c5b1d5424eac357b823272527e34ab0
Carol Xie <carolx@berkeley.edu>
2019-08-02 14:31:13 +0800

    basic visualized test done

--- Matcher/FeatureMatchTest.java
+++ Matcher/FeatureMatchTest.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class FeatureMatchTest {
@@ -10,13 +11,20 @@ public class FeatureMatchTest {
         }
 
         User user1 = sportyUser(100);
+        User user2 = artsyUser(101);
 
-        int bestMatch = new FeatureMatch(user1, adList).bestAdID();
+        int bestMatch1 = new FeatureMatch(user1, adList).bestAdID();
+        int bestMatch2 = new FeatureMatch(user2, adList).bestAdID();
 
         // visualized test
-        System.out.println("Best matching id is " + bestMatch);
-        adList[bestMatch - 100].getFeatureMap().toString();
+        System.out.println("Best matching id for the sportyUser is " + bestMatch1);
+        printFeatureMap(adList[bestMatch1 - 100].getFeatureMap());
+
+        System.out.println("Best matching id for the artsyUser is " + bestMatch2);
+        printFeatureMap(adList[bestMatch2 - 100].getFeatureMap());
     }
+
+    /// random feature map utilities
 
     private HashMap<String, Double> randomizedMap() {
         HashMap<String, Double> result = new HashMap<>();
@@ -26,6 +34,15 @@ public class FeatureMatchTest {
         return result;
     }
 
+    private void printFeatureMap(HashMap<String, Double> map) {
+        for (Map.Entry<String, Double> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+
+    /// random user utility
+
     private User sportyUser(int Id) {
         User user = new User(Id);
         user.updateFeature("sport", 0.8);
@@ -33,7 +50,18 @@ public class FeatureMatchTest {
         user.updateFeature("home", 0.4);
         user.updateFeature("computer", 0.9);
         user.updateFeature("dress", 0);
-        user.updateFeature("computer", 0.6);
+        user.updateFeature("coffee", 0.5);
+        return user;
+    }
+
+    private User artsyUser(int Id) {
+        User user = new User(Id);
+        user.updateFeature("sport", 0.1);
+        user.updateFeature("cosmetics", 0.9);
+        user.updateFeature("home", 0.4);
+        user.updateFeature("computer", 0.3);
+        user.updateFeature("dress", 0.6);
+        user.updateFeature("cofee", 0.7);
         return user;
     }
 }
